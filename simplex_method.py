fbe458c189145f4331389f8dffb837d912695d57
Hiroaki-K4 <49no19@gmail.com>
2023-08-20 11:08:22 +0900

    Add simplex method

---
+++ simplex_method.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def simplex_method(input_mat):
+    print(input_mat)
+
+
+if __name__ == "__main__":
+    input_mat = np.array([[1, 60, -2, -8], [1, 60, -4, -4], [0, 0, 29, 45]])
+    simplex_method(input_mat)
